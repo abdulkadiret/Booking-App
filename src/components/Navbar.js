68614388db88692b7ad770db3d0c70ce115fca57
abdulkadiret <ethiogentelman@gmail.com>
2020-06-24 13:51:04 +0100

    Create Navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+const NavigationBar = () => {
+  return (
+    <Navbar collapseOnSelect expand="sm" bg="primary" variant="dark">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link as={NavLink} to="/">
+            Booking
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/seating">
+            Seating
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
