4d3f01a859836adc5eb3f059faceffc0196ea56a
abdulkadiret <ethiogentelman@gmail.com>
2020-07-06 01:59:10 +0100

    Create a booking form

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,37 +1,104 @@
-import React from "react";
-import DATA from "../data/bookingData.json";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Booking = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <Container className="table-responsive">
-      <h1 className="text-center">Booking</h1>
-      <table className="table table-hover">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Dining Data</th>
-            <th scope="col">Number of Covers</th>
-            <th scope="col">phone number</th>
-            <th scope="col">Email</th>
-          </tr>
-        </thead>
-        {DATA.map((data) => {
-          return (
-            <tbody>
-              <tr>
-                <td>{data.firstName}</td>
-                <td>{data.lastName}</td>
-                <td>{data.diningDate}</td>
-                <td>{data.numberOfCovers}</td>
-                <td>{data.phoneNumber}</td>
-                <td>{data.email}</td>
-              </tr>
-            </tbody>
-          );
-        })}
-      </table>
+    <Container className="d-flex flex-column align-items-center mt-5">
+      <>
+        <h3>Make Your Booking Here</h3>
+        <p>It's quick and easy</p>
+      </>
+      <form
+        class="form-group d-flex flex-column justify-content-center"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="form-group d-flex">
+          <div className="col-md-6">
+            <label for="firstName" className="text-muted">
+              First Name:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="firstName"
+              id="firstName"
+              ref={register({ required: true })}
+            />
+          </div>
+          <div className="col-md-6">
+            <label for="lastName" className="text-muted">
+              Last Name:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="lastName"
+              id="lastName"
+              ref={register({ required: true })}
+            />
+          </div>
+        </div>
+        <div className="form-group d-flex justify-content-center">
+          <div className="col-md-4">
+            <label for="quantity" className="text-muted">
+              Number of people:
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              name="quantity"
+              id="quantity"
+              ref={register({ required: true })}
+            />
+          </div>
+          <div className="col-md-4 mr-auto ml-auto">
+            <label for="date-picker" className="text-muted">
+              Pick date:
+            </label>
+            <input
+              type="date"
+              className="form-control"
+              name="datePicker"
+              id="datePicker"
+              ref={register({ required: true })}
+            />
+          </div>
+        </div>
+        <div className="form-group d-flex">
+          <div className="col-sm-6">
+            <label for="phone" className="text-muted">
+              Phone Number:
+            </label>
+            <input
+              type="tel"
+              className="form-control"
+              name="phone"
+              id="phone"
+              ref={register({ required: true })}
+            />
+          </div>
+          <div className="col-sm-6">
+            <label for="email" className="text-muted">
+              Email:{" "}
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              id="email"
+              ref={register({ required: true })}
+            />
+          </div>
+        </div>
+        <input
+          type="submit"
+          className="p-2 m-3 bg-primary text-white border-0 rounded"
+        />
+      </form>
     </Container>
   );
 };
