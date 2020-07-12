b8cb757a92df75859e6e6e285088c8da46ac8ccb
abdulkadiret <ethiogentelman@gmail.com>
2020-07-12 18:34:46 +0100

    Create route for myBooking component

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Booking from "../components/Booking";
-import Seating from "../components/Seating";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Booking from '../components/Booking';
+import Seating from '../components/Seating';
+import MyBooking from '../components/MyBooking';
 
 export default () => (
   <Switch>
     <Route exact path="/" component={Booking} />
     <Route path="/seating" component={Seating} />
+    <Route path="/myBooking" component={MyBooking} />
   </Switch>
 );
